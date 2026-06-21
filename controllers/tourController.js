eb28d27160a4f098a49d97d0a25e9d3bb43581e1
prak-coder <prakash.change@gmail.com>
2026-06-21 15:08:08 +0530

    create get a specific tour by id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,64 +1,70 @@
-const prisma = require('../utils/db')
+const prisma = require("../utils/db");
 
-exports.getAllTours = async(req,res) => {
-    try {
-        const tours = await prisma.tour.findMany();
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await prisma.tour.findMany();
 
-        res.status(200).json({
-        status:'success',
-        numberoftours:tours.length,
-        data:{
-            tours
-        }   })
-    } catch (error) {
-        res.status(400).json({
-            status:'fail',
-            message:error.message
-        })
-        
-    }
-}
+    res.status(200).json({
+      status: "success",
+      numberoftours: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
 
-exports.getTour = async(req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await prisma.tour.findFirst({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
 
-exports.createTour = async(req,res) => {
+exports.createTour = async (req, res) => {
+  try {
+    const tour = await prisma.tour.create({ data: req.body });
 
-    try {
-    const tour =await prisma.tour.create({data:req.body})
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
 
-         res.status(200).json({
-        status:'success',
-        data:{
-            tour
-        }   
-        })
-        
-    } catch (error) {
-        res.status(400).json({
-            status:'fail',
-            message:error.message
-        })
-    }
-}
+exports.updateTour = async (req, res) => {
+  try {
+  } catch (error) {}
+};
 
-exports.updateTour = async(req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
-
-exports.deleteTour = async(req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+exports.deleteTour = async (req, res) => {
+  try {
+  } catch (error) {}
+};
